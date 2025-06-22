3bb8a09a951f8926a035c64dfd94bb8d5e6a1d4f
Prateek Kumar <prateekkum990@gmail.com>
2025-06-22 18:07:01 +0530

    model and schema definitions
    created sqlalchemy models and pydantic models for user, project, code file, code block and narrations

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,6 @@
-from fastapi import FastAPI, UploadFile, File, HTTPException
-from app.schema.input import CodeFileInput
-from app.services import parsing
-import os
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from routes import input
 
 app = FastAPI()
 
@@ -15,35 +13,9 @@ app.add_middleware(
 )
 
 
+
 @app.get("/")
 def read_root():
     return {"message": "Welcome to the SilentScribe!"}
 
-@app.post("/upload")
-async def upload_file(files: list[UploadFile] = File(...)):
-    """
-    Upload endpoint to receive code file, validate it, and prepare for narration.
-    """
-    result = []
-    for file in files:
-        filename = file.filename
-        contents = await file.read()
-
-        try:
-            model = CodeFileInput(
-                file_name=filename,
-                file_type=os.path.splitext(filename)[-1][1:],
-                file_content=contents.decode("utf-8")
-            )
-        except Exception as e:
-            raise HTTPException(status_code=400, detail=f"Validation error: {e}")
-
-        structure = parsing.extract_ast_metadata(model.file_content)
-
-        result.append({
-            "message": "File accepted",
-            "file_name": model.file_name,
-            "type": model.file_type,
-            "structure": structure
-        })
-    return {"message":"files have been processed", "result": result}
+app.include_router(input.router)
---
+++ backend/app/models.py
@@ -0,0 +1,69 @@
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, Text, JSON, ForeignKey
+from sqlalchemy.orm import relationship
+from app.database import Base
+from datetime import datetime
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    full_name = Column(String, nullable = False)
+    is_active = Column(Boolean, default=True)
+    created_at = Column(DateTime, default=datetime.now().isoformat())
+
+    projects = relationship("Project", back_populates = "owner")
+
+class Project(Base):
+    __tablename__ = "projects"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, nullable = False)
+    owner_id = Column(Integer, ForeignKey('users.id'))
+    description = Column(String, nullable=True)
+
+    owner = relationship("User", back_populates="projects")
+    files = relationship("CodeFiles", back_populates ="project")
+
+class CodeFiles(Base):
+    __tablename__ = "code_files"
+
+    id = Column(Integer, primary_key=True, index=True)
+    file_name = Column(String, nullable=False)
+    file_content = Column(String, nullable=False)
+    project_id = Column(Integer, ForeignKey('projects.id'))
+    created_at = Column(DateTime, default=datetime.now().isoformat())
+    
+    project = relationship("Project", back_populates="files")
+    code_blocks = relationship("CodeBlock", back_populates="code_file")
+
+class CodeBlock(Base):
+    __tablename__ = "code_blocks"
+
+    id = Column(Integer, primary_key=True, index=True)
+    code_type = Column(String, nullable=False)
+    name = Column(String, nullable=True)
+    lineno = Column(Integer)
+    col_offset = Column(Integer)
+    end_lineno = Column(Integer)
+    end_col_offset = Column(Integer)
+    docstring = Column(String, nullable=True)
+    used_names = Column(JSON, default = [])
+    args = Column(JSON, default = [])
+    returns = Column(String, nullable=True)
+    code = Column(Text, nullable=False)
+    file_id = Column(Integer, ForeignKey('code_files.id'))
+
+    code_file = relationship("CodeFiles", back_populates="code_blocks")
+    narration = relationship("Narration", back_populates="code_block")
+
+class Narration(Base):
+    __tablename__ ="narrations"
+
+    id = Column(Integer, primary_key=True, index=True)
+    script = Column(Text, nullable=False)
+    created_at = Column(DateTime, default=datetime.now().isoformat())
+    code_block_id = Column(Integer, ForeignKey('code_blocks.id'))
+
+    code_block = relationship("CodeBlock", back_populates="narration")
